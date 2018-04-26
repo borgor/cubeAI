b0ec3b99a9d50f84aa479ff5b96a3f53f5295771
Boris Gorshenev <bgorshen@ucsc.edu>
2018-04-25 23:33:08 -0700

    Fixed cube translation orientation, added sequence generator.

--- Dataset_generation/compatibility.py
+++ Dataset_generation/compatibility.py
@@ -5,42 +5,71 @@ from MagicCube.code import cube as renderer
 
 import numpy as np
 
+#To make things awful, the render and solver disagree on cube orientation.
+#This dictionary translates Solver Faces to Renderer Faces
+#faceTranslate= {"U":"D", "D":"U", "B":"R", "F":"L", "R":"B", "L":"F"}
+#For reference, here is the dict used in the renderer
+#facedict = {"U":0, "D":1, "F":2, "B":3, "R":4, "L":5}
+
+#Therefore, this combines the two into a single table
+fd = {Color.D:0, Color.U:1, Color.L:2, Color.R:3, Color.B:4, Color.F:5}
+
 def cubie_to_stickers(solvercube):
     #translate the solver's representation to an array of stickers for the renderer
     fc = solvercube.to_facelet_cube()
     s = []
     for i in range(54):
-        if fc.f[i] == Color.U:
-            s.append(0)
-        elif fc.f[i] == Color.R:
-            s.append(4)
-        elif fc.f[i] == Color.F:
-            s.append(2)
-        elif fc.f[i] == Color.D:
-            s.append(1)
-        elif fc.f[i] == Color.L:
-            s.append(5)
-        elif fc.f[i] == Color.B:
-            s.append(3)
+        #Substitutes colors, so that the Solver's orientation is preserved (i.e. solver-up is rendered as up)
+        s.append(fd[fc.f[i]])
+        
+#        if fc.f[i] == Color.U:
+#            s.append(1)
+#        elif fc.f[i] == Color.R:
+#            s.append(3)
+#        elif fc.f[i] == Color.F:
+#            s.append(5)
+#        elif fc.f[i] == Color.D:
+#            s.append(0)
+#        elif fc.f[i] == Color.L:
+#            s.append(2)
+#        elif fc.f[i] == Color.B:
+#            s.append(4)
 
     a = np.array([
-        [s[0:3],s[3:6],s[6:9]], #U
-        [s[27:30], s[30:33], s[33:36]], #D
-        [s[18:21],s[21:24],s[24:27]], #F
-        [s[45:48],s[48:51],s[51:54]], #B
-        [s[9:12],s[12:15],s[15:18]], #R
-        [s[36:39],s[39:42],s[42:45]], #L
+        #SolverName to RendererName
+        np.rot90([s[0:3],s[3:6],s[6:9]],-1), #U
+        np.rot90([s[27:30], s[30:33], s[33:36]],-1), #D
+        np.rot90([s[18:21],s[21:24],s[24:27]],-1), #F
+        np.rot90([s[45:48],s[48:51],s[51:54]],-1), #B
+        np.rot90([s[9:12],s[12:15],s[15:18]],-1), #R
+        np.rot90([s[36:39],s[39:42],s[42:45]],-1), #L
         ])
     return a
 
-cubie.CubieCube.to_stickers = cubie_to_stickers
 
 
 
-def rendercubie(self):
+
+def renderCubie(cb):
     rc = renderer.Cube(3)
-    rc.stickers = self.to_stickers()
+    rc.stickers = cubie_to_stickers(cb)
     img = rc.render()
     return img
 
-cubie.CubieCube.render = rendercubie
+
+def solveCubie(cb, animate = False):
+    rc = renderer.Cube(3)
+    rc.stickers = cubie_to_stickers(cb)
+    img = rc.render()
+    cs = cb.to_facelet_cube().__str__()
+    try:
+        sol = sv.solve(cs)
+    except IndexError:
+        pass
+    print(sol)
+    if animate:
+        for i in range(0,len(sol)-5,3):
+            rc.move(sol[i],0,int(sol[i+1]))
+            rc.render()
+    return sol[:-5]
+
